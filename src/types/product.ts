3fd75622a7873db2f41b7cd631fd106334ac142c
HinaZeeshan <147984993+HinaZeeshan@users.noreply.github.com>
2025-01-19 14:32:36 +0500

    Update product.ts

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,12 +1,13 @@
+// src/interfaces/product.ts
 
-
-export type  Product= {
-    slug: string;
-    title: string;
-    description: string;
-    imageUrl: string; // Direct URL for the product image
-    price: number;
-    tags?: string[];
-    discountPercentage?: number;
-    isNew?: boolean;
+export type Product = {
+    title: string; // The title of the product
+    slug: string; // The slug (URL-friendly identifier) of the product
+    description: string; // Description of the product
+    imageUrl: string; // URL of the product image
+    price: number; // Price of the product
+    tags?: string[]; // Optional array of tags associated with the product
+    discountPercentage?: number; // Optional discount percentage for the product
+    isNew?: boolean; // Optional flag indicating if the product is new
   }
+  
