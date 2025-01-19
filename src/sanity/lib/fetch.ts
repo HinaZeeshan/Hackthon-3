0badf0ae72ab173125d54b3612ab9249ac209523
HinaZeeshan <147984993+HinaZeeshan@users.noreply.github.com>
2025-01-19 14:45:21 +0500

    move

--- src/sanity/lib/fetch.ts
+++ src/sanity/lib/fetch.ts
@@ -1,16 +1,34 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-
 import { createClient } from "next-sanity";
+
+const client = createClient({
+    projectId: "ibyygf5d",
+    dataset: "production",
+    useCdn: true,
+    apiVersion: "2023-10-10"
+});
+
+export async function sanityFetch({
+    querry,
+    params = {},
+}: {
+    querry: string;
+    params?: Record<string, unknown>;
+}) {
+    return await client.fetch(querry, params);
+}
+
+
+
+// import { createClient } from "next-sanity";
 
  
 
-const client = createClient ({
-    projectId : "ibyygf5d",
-    dataset : "production",
-    useCdn : true,
-    apiVersion : "2023-10-10"
-})
-export async function sanityFetch({querry,params ={}} :{querry: string, params?: any}){
-return await client.fetch(querry,params) 
-}
+// const client = createClient ({
+//     projectId : "ibyygf5d",
+//     dataset : "production",
+//     useCdn : true,
+//     apiVersion : "2023-10-10"
+// })
+// export async function sanityFetch({querry,params ={}} :{querry: string, params?: any}){
+// return await client.fetch(querry,params) 
+// }
